1894b75137b16cb8108382448a4f798d9cfe00ea
Andreza Raquel <andreza.queiroz@ccc.ufcg.edu.br>
2017-05-31 10:08:54 -0300

    Removing header from csv files

--- BigSeaSparkGeomatching/src/main/java/LineMatching20/RunBULMAv2.java
+++ BigSeaSparkGeomatching/src/main/java/LineMatching20/RunBULMAv2.java
@@ -1,14 +1,12 @@
 package LineMatching20;
 
-import LineDependencies.GeoLine;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import scala.Tuple2;
 
-import java.util.Iterator;
+import LineDependencies.GeoLine;
+import scala.Tuple2;
 
 public class RunBULMAv2 {
 
@@ -18,7 +16,7 @@ public class RunBULMAv2 {
 			System.exit(1);
 		}
 
-		Long tempoInicial = System.currentTimeMillis();
+		Long initialTime = System.currentTimeMillis();
 		
 		String pathFileShapes = args[0];
 		String pathGPSFile = args[1];
@@ -26,36 +24,49 @@ public class RunBULMAv2 {
 		int minPartitions = Integer.valueOf(args[3]);
 		
 		
-//		SparkConf sparkConf = new SparkConf().setAppName("GeoMatchingSpark").setMaster("local");
 		SparkSession spark = SparkSession
 				  .builder()
 				  .master("local")
 				  .config("spark.some.config.option", "some-value")
 				  .config("spark.sql.warehouse.dir", "file:///tmp/spark-warehouse")
 				  .getOrCreate();
-		
-		Function2<Integer, Iterator<String>, Iterator<String>> removeHeader = new Function2<Integer, Iterator<String>, Iterator<String>>() {
-			@Override
-			public Iterator<String> call(Integer index, Iterator<String> iterator) throws Exception {
-				if (index == 0 && iterator.hasNext()) {
-					iterator.next();
-					return iterator;
-				} else {
-					return iterator;
-				}
-			}
-		};
-
-		JavaSparkContext ctx = new JavaSparkContext(spark.sparkContext());
-
+			
 		Dataset<Row> datasetGPSFile = spark.read().text(pathGPSFile);
-		Dataset<Row> datasetShapeFile = spark.read().text(pathFileShapes);
+		Row headerGPSFile = datasetGPSFile.first();
+		datasetGPSFile = datasetGPSFile.filter(new FilterFunction<Row>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean call(Row value) throws Exception {
+				if (value.equals(headerGPSFile)) {
+					return false;
+				}
+				return true;
+			}
+		});
 		
-		Dataset<Tuple2<String, GeoLine>> lines = MatchingRoutesV2.generateDataFrames(datasetShapeFile, datasetGPSFile, minPartitions, spark);
+		
+		Dataset<Row> datasetShapesFile = spark.read().text(pathFileShapes);
+		Row headerShapesFile = datasetShapesFile.first();
+		datasetShapesFile = datasetShapesFile.filter(new FilterFunction<Row>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean call(Row value) throws Exception {
+				if (value.equals(headerShapesFile)) {
+					return false;
+				}
+				return true;
+			}
+		});
+		
+		Dataset<Tuple2<String, GeoLine>> lines = MatchingRoutesV2.generateDataFrames(datasetShapesFile, datasetGPSFile, minPartitions, spark);
 		Dataset<String> output = MatchingRoutesV2.run(lines,minPartitions, spark);
 		output.toJavaRDD().saveAsTextFile(pathOutput);
 		
-		System.out.println("Execution time with Dataset: " + (System.currentTimeMillis() - tempoInicial));
+		System.out.println("Execution time with Dataset: " + (System.currentTimeMillis() - initialTime));
 		
 	}
 
