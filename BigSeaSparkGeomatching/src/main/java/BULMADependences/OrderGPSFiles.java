e9f91a908accbe0b8b22d58728e0da22b74295bb
Andreza Raquel <andreza.queiroz@ccc.ufcg.edu.br>
2017-07-26 11:13:18 -0300

    Renaming class

--- BigSeaSparkGeomatching/src/main/java/BULMADependences/OrderGPSFiles.java
+++ BigSeaSparkGeomatching/src/main/java/BULMADependences/OrderGPSFiles.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class PreProcessing {
+public class OrderGPSFiles {
 
 	public static void main(String[] args) throws Exception {
 
